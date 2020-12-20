3427bb04a387f32df68d491ee27ae7128e7e18fa
Filip Wójtowicz <fwojtowiczagh@gmail.com>
2020-12-20 15:49:48 +0100

    Uzupełnienie funkcji

--- include/types.hpp
+++ include/types.hpp
@@ -5,6 +5,17 @@
 #ifndef NET_SIMULATION_TYPES_HPP
 #define NET_SIMULATION_TYPES_HPP
 
+class ElementID {
+public:
+    ElementID() = default;
+    ElementID(int id) : _id(id) {}
+
+    int get_id() const { return _id; }
+
+private:
+    int _id;
+};
+
 #endif //NET_SIMULATION_TYPES_HPP
 
 //aliasy
